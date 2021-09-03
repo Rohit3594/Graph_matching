48e1cd11d155346f09d3b61f2c2eaa513bd9d631
Rohit <yadavrohit3594@gmail.com>
2021-09-03 19:00:19 +0200

    changes to ground_truth_visu_with_outliers

--- visu_simulations/ground_truth_visu_with_outliers.py
+++ visu_simulations/ground_truth_visu_with_outliers.py
@@ -10,21 +10,22 @@ if __name__ == "__main__":
 	file_template_mesh = '../data/template_mesh/lh.OASIS_testGrp_average_inflated.gii'
 	file_sphere_mesh = '../data/template_mesh/ico100_7.gii'
 	simus_run = 0
-	path_to_graphs = '../data/simu_graph/0/test/0/noise_1400,outliers_8/graphs'
+	path_to_graphs = '../data/simu_graph/0/test/0/noise_200,outliers_8/graphs'
 	list_graphs = gp.load_graphs_in_list(path_to_graphs)
 
-	gt = np.load('../data/simu_graph/0/test/0/noise_1400,outliers_8/ground_truth.npy')
+	gt = np.load('../data/simu_graph/0/test/0/noise_200,outliers_8/ground_truth.npy')
 
 	gt_01 = gt[0][1]
-	# gt_02 = gt[0][2]
-	# gt_03 = gt[0][3]
-	# gt_04 = gt[0][4]
+	gt_02 = gt[0][2]
+	gt_03 = gt[0][3]
+	gt_04 = gt[0][4]
 
 	g = list_graphs[0]
 	g1 = list_graphs[1]
-	# g2 = list_graphs[2]
-	# g3 = list_graphs[3]
-	# g4 = list_graphs[4]
+	g2 = list_graphs[2]
+	g3 = list_graphs[3]
+	g4 = list_graphs[4]
+
 
 	dict_lab = {}
 	for node in g1.nodes:
@@ -36,32 +37,32 @@ if __name__ == "__main__":
 
 
 
-	# dict_lab = {}
-	# for node in g2.nodes:
-	# 	if node >=20:
-	# 		dict_lab[node] = {'label':20}
-	# 	else:
-	# 		dict_lab[gt_02[node]] = {'label':node}
-	# nx.set_node_attributes(g2,dict_lab)
+	dict_lab = {}
+	for node in g2.nodes:
+		if node >=20:
+			dict_lab[node] = {'label':20}
+		else:
+			dict_lab[gt_02[node]] = {'label':node}
+	nx.set_node_attributes(g2,dict_lab)
 
 
-	# dict_lab = {}
-	# for node in g3.nodes:
-	# 	if node >=20:
-	# 		dict_lab[node] = {'label':20}
-	# 	else:
-	# 		dict_lab[gt_03[node]] = {'label':node}
-	# nx.set_node_attributes(g3,dict_lab)
+	dict_lab = {}
+	for node in g3.nodes:
+		if node >=20:
+			dict_lab[node] = {'label':20}
+		else:
+			dict_lab[gt_03[node]] = {'label':node}
+	nx.set_node_attributes(g3,dict_lab)
 
 
 
-	# dict_lab = {}
-	# for node in g4.nodes:
-	# 	if node >=20:
-	# 		dict_lab[node] = {'label':20}
-	# 	else:
-	# 		dict_lab[gt_04[node]] = {'label':node}
-	# nx.set_node_attributes(g4,dict_lab)
+	dict_lab = {}
+	for node in g4.nodes:
+		if node >=20:
+			dict_lab[node] = {'label':20}
+		else:
+			dict_lab[gt_04[node]] = {'label':node}
+	nx.set_node_attributes(g4,dict_lab)
 
 
 
@@ -78,40 +79,24 @@ if __name__ == "__main__":
 	sphere_mesh = sio.load_mesh(file_sphere_mesh)
 	mesh = gv.reg_mesh(sio.load_mesh(file_template_mesh))
 
-	gp.sphere_nearest_neighbor_interpolation(g, sphere_mesh)
-	gp.sphere_nearest_neighbor_interpolation(g1, sphere_mesh)
-	# gp.sphere_nearest_neighbor_interpolation(g2, sphere_mesh)
-	# gp.sphere_nearest_neighbor_interpolation(g3, sphere_mesh)
-	# gp.sphere_nearest_neighbor_interpolation(g4, sphere_mesh)
+
+	# Stacking Graphs
+	concat_graphs = nx.disjoint_union(g,g1)
+	graph_stack = [g2,g3,g4]
+
+	for graph in graph_stack:
+
+		concat_graphs = nx.disjoint_union(concat_graphs,graph)
 
 
-	nodes_coords = gp.graph_nodes_to_coords(g, 'ico100_7_vertex_index', sphere_mesh)
-	nodes_coords_1 = gp.graph_nodes_to_coords(g1, 'ico100_7_vertex_index', sphere_mesh)
-	# nodes_coords_2 = gp.graph_nodes_to_coords(g2, 'ico100_7_vertex_index', sphere_mesh)
-	# nodes_coords_3 = gp.graph_nodes_to_coords(g3, 'ico100_7_vertex_index', sphere_mesh)
-	# nodes_coords_4 = gp.graph_nodes_to_coords(g4, 'ico100_7_vertex_index', sphere_mesh)
+
+	gp.sphere_nearest_neighbor_interpolation(concat_graphs, sphere_mesh)
+
+	nodes_coords = gp.graph_nodes_to_coords(concat_graphs, 'ico100_7_vertex_index', sphere_mesh)
 
 
 	vb_sc = gv.visbrain_plot(sphere_mesh, caption='Visu of simulated graph on template mesh')
-	s_obj, c_obj, node_cb_obj = gv.show_graph(g, nodes_coords,node_color_attribute='label')
-	vb_sc.add_to_subplot(s_obj)
-	#vb_sc.add_to_subplot(c_obj)
-	
-	s_obj, c_obj, node_cb_obj = gv.show_graph(g1, nodes_coords_1,node_color_attribute='label')
-	#vb_sc.add_to_subplot(c_obj)
+	s_obj, c_obj, node_cb_obj = gv.show_graph(concat_graphs, nodes_coords,node_color_attribute='label')
 	vb_sc.add_to_subplot(s_obj)
 
-	# s_obj, c_obj, node_cb_obj = gv.show_graph(g2, nodes_coords_2,node_color_attribute='label')
-	# #vb_sc.add_to_subplot(c_obj)
-	# vb_sc.add_to_subplot(s_obj)
-
-	# s_obj, c_obj, node_cb_obj = gv.show_graph(g3, nodes_coords_3,node_color_attribute='label')
-	# #vb_sc.add_to_subplot(c_obj)
-	# vb_sc.add_to_subplot(s_obj)
-
-	# s_obj, c_obj, node_cb_obj = gv.show_graph(g4, nodes_coords_4,node_color_attribute='label')
-	# #vb_sc.add_to_subplot(c_obj)
-	# vb_sc.add_to_subplot(s_obj)
-
-	# show the plot on the screen
 	vb_sc.preview()
