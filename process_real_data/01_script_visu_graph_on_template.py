15d3bec8db95860fa311dc4f246f6b06bda3eb4f
gauzias <guillaume.auzias@univ-amu.fr>
2022-11-07 17:55:50 +0100

    new fig

--- process_real_data/01_script_visu_graph_on_template.py
+++ process_real_data/01_script_visu_graph_on_template.py
@@ -7,26 +7,26 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    template_mesh = '../data/template_mesh/lh.OASIS_testGrp_average_inflated.gii'
+    template_mesh = '../data/template_mesh/OASIS_avg.lh.white.talairach.reg.ico7.inflated.gii'#lh.OASIS_testGrp_average_inflated.gii'
+    mesh = gv.reg_mesh(sio.load_mesh(template_mesh))
     # template_mesh = '/home/rohit/PhD_Work/GM_my_version/Graph_matching/data/template_mesh/lh.OASIS_testGrp_average_inflated.gii'
     # path_to_graphs = '/home/rohit/PhD_Work/GM_my_version/Graph_matching/data/OASIS_full_batch/modified_graphs/'
-    path_to_graphs = '../data/OASIS_full_batch/modified_graphs/'
+    path_to_graphs = '../data/Oasis_original_new_with_dummy/modified_graphs'
     file_sphere_mesh = '../data/template_mesh/ico100_7.gii'
     list_graphs = gp.load_graphs_in_list(path_to_graphs)
 
     # Get the mesh
-    mesh = gv.reg_mesh(sio.load_mesh(template_mesh))
     sphere_mesh = sio.load_mesh(file_sphere_mesh)
     mask_slice_coord = -15
     vb_sc = None
     inds_to_show = [0,1,11]
     graphs_to_show=[list_graphs[i] for i in inds_to_show]
     for g in graphs_to_show:
-        gp.sphere_nearest_neighbor_interpolation(g, sphere_mesh)
-
+        # gp.sphere_nearest_neighbor_interpolation(g, sphere_mesh)
+        #
         nodes_to_remove = gp.remove_dummy_nodes(g)
-        nodes_to_remove = np.where(np.array(nodes_to_remove)==False)
-        g.remove_nodes_from(list(nodes_to_remove[0]))
+        # nodes_to_remove = np.where(np.array(nodes_to_remove)==False)
+        # g.remove_nodes_from(list(nodes_to_remove[0]))
 
         nodes_coords = gp.graph_nodes_to_coords(g, 'ico100_7_vertex_index', mesh)
         nodes_mask = nodes_coords[:,2]>mask_slice_coord
@@ -42,3 +42,16 @@ if __name__ == "__main__":
     # vb_sc.add_to_subplot(s_obj)
     # vb_sc.add_to_subplot(c_obj)
     vb_sc.preview()
+
+
+    sphere_mesh = sio.load_mesh(file_sphere_mesh)
+    vb_sc2 = gv.visbrain_plot(sphere_mesh)
+    for g in list_graphs:
+        nodes_coords = gp.graph_nodes_to_coords(g, 'ico100_7_vertex_index', sphere_mesh)
+        #nodes_mask = nodes_coords[:,2]>mask_slice_coord
+        s_obj, nodes_cb_obj = gv.graph_nodes_to_sources(nodes_coords)
+
+        vb_sc2.add_to_subplot(s_obj)
+
+    vb_sc2.preview()
+
